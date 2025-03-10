633259a9ba04b7d72c87353d75356f746c54e2e4
shaun <modipanesh@gmail.com>
2025-03-10 15:52:43 +0200

    feat(ui): add ShadCN button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-	return <div className="">Hello world</div>;
+	return (
+		<main className="h-full w-full flex flex-col gap-y-5 items-center justify-center p-2">
+			<h1>HackerMeet</h1>
+			<Button className="bg-white" variant="default" size={'lg'}>
+				Sign
+			</Button>
+		</main>
+	);
 }
