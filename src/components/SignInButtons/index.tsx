d0d5f1284f1d77ee2054daa03af637c99be1c33c
shaun <modipanesh@gmail.com>
2025-03-10 17:06:36 +0200

    feat: added sign-in authentication

--- src/components/SignInButtons/index.tsx
+++ src/components/SignInButtons/index.tsx
@@ -2,10 +2,11 @@
 
 import React from 'react';
 import { Button } from '../ui/button';
+import { signIn } from 'next-auth/react';
 
 const SignInButton = () => {
 	return (
-		<Button className="bg-white" variant="default" size={'lg'}>
+		<Button className="bg-white" variant="default" size={'lg'} onClick={() => signIn('google')}>
 			Sign In
 		</Button>
 	);
